32cd55c6065f7d7ef0920b1afca7400961a589e8
robertfoenix <38993572+robertfoenix@users.noreply.github.com>
2025-05-11 03:38:55 +0300

    fix box-shadow not rendering

--- novus-client/src/components/ManVSMainMenu.vue
+++ novus-client/src/components/ManVSMainMenu.vue
@@ -24,6 +24,7 @@ export default {
   height: 59px;
   box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.08);
   z-index: 1; /* for box-shadow to show */
+  position: relative; /* for z-index to work */
   background-color: #ffffff;
   padding-top: clamp(17px, calc(19.806px - 0.1948vw), 19px); /* 19px @414 → 17px @1440 */
   padding-bottom: clamp(16px, calc(18.806px - 0.1948vw), 18px); /* 18px @414 → 16px @1440 */
